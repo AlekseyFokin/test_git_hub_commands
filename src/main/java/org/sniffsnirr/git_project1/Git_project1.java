8eeb3a8c2d09ef282479677f66b6827839580698
Aleksey Fokin <alfo12@mail.ru>
2023-11-09 11:50:57 +0300

    first commit

---
+++ src/main/java/org/sniffsnirr/git_project1/Git_project1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.sniffsnirr.git_project1;
+
+/**
+ *
+ * @author sniffsnirr
+ */
+public class Git_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
