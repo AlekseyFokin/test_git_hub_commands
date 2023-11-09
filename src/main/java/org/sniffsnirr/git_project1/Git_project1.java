dfa42664b70d03f2cdc2245743fba517fdc93667
Aleksey Fokin <alfo12@mail.ru>
2023-11-09 22:27:32 +0300

    after merge

--- src/main/java/org/sniffsnirr/git_project1/Git_project1.java
+++ src/main/java/org/sniffsnirr/git_project1/Git_project1.java
@@ -11,6 +11,11 @@ package org.sniffsnirr.git_project1;
 public class Git_project1 {
 
     public static void main(String[] args) {
+
         System.out.println("Marge World!");
+        System.out.println("Hello Merge!");
+
+      
+
     }
 }
